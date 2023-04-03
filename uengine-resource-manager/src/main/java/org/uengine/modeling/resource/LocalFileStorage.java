d4d9fed1a01a20b6dffbff3e54120eb540223f25
Jinyoung Jang <jyjang@uengine.org>
2023-04-03 21:58:04 +0000

    add uengine-resource-manager too

---
+++ uengine-resource-manager/src/main/java/org/uengine/modeling/resource/LocalFileStorage.java
@@ -0,0 +1,166 @@
+package org.uengine.modeling.resource;
+
+import org.apache.commons.io.FileUtils;
+import org.metaworks.MetaworksContext;
+import org.oce.garuda.multitenancy.TenantContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.uengine.modeling.IModel;
+import static java.nio.file.StandardCopyOption.*;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by jangjinyoung on 15. 7. 12..
+ */
+@Component
+public class LocalFileStorage extends AbstractStorage{
+
+    @Override
+    public void delete(IResource fileResource) throws IOException {
+        File file = getFile(fileResource);
+
+        if(file.isDirectory()){
+            FileUtils.deleteDirectory(file);
+        }else{
+            file.delete();
+
+        }
+    }
+
+    @Override
+    public void rename(IResource fileResource, String newName) {
+        getFile(fileResource).renameTo(new File(getTenantBasePath() + newName));
+    }
+
+    @Override
+    public void copy(IResource src, String desPath) throws IOException {
+        File destinationFile = new File(getTenantBasePath() + desPath);
+        File sourceFile = getFile(src);
+
+        if(sourceFile.isDirectory()){
+            FileUtils.copyDirectory(getFile(src), destinationFile);
+        }else{
+            destinationFile.getParentFile().mkdirs();
+
+            if(isDoNotOverwrite())
+                Files.copy(getFile(src).toPath(), destinationFile.toPath());
+            else
+                Files.copy(getFile(src).toPath(), destinationFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        }
+
+    }
+
+    @Override
+    public List<IResource> listFiles(IContainer containerResource) throws Exception {
+        List<IResource> resourceList = new ArrayList<IResource>();
+
+        File directory = getFile(containerResource);
+
+        String tenantBasePath = getTenantBasePath();
+
+        File tenantBase = new File(tenantBasePath);
+        if(!tenantBase.exists()){
+            tenantBase.mkdirs();
+        }
+
+        String abstractTenantBasePath = new File(tenantBasePath).getAbsolutePath();
+
+        if(directory!=null && directory.exists())
+        for(File file : directory.listFiles()){
+
+            if(file.getName().startsWith(".")) continue;
+
+            String relativePath = file.getAbsolutePath().replace("\\", "/");
+
+            relativePath = relativePath.substring(abstractTenantBasePath.length() + 1);
+
+            if(file.isDirectory()){
+                ContainerResource subContainerResource = (ContainerResource) containerResource.getClass().newInstance();
+
+                subContainerResource.setPath(relativePath);
+                subContainerResource.setMetaworksContext(new MetaworksContext());
+
+//                if(false)
+//                    subContainerResource.setChildren(listFiles(subContainerResource));
+
+                resourceList.add(subContainerResource);
+            }else{
+                resourceList.add(DefaultResource.createResource(relativePath));
+            }
+        }
+
+        return resourceList;
+    }
+
+    @Override
+    public void createFolder(IContainer containerResource) throws Exception {
+        File directory = getFile(containerResource);
+
+        directory.mkdirs();
+    }
+
+    @Override
+    public boolean exists(IResource resource) throws Exception {
+        return getFile(resource).exists();
+    }
+
+    @Override
+    public Object getObject(IResource resource) throws Exception {
+        return Serializer.deserialize(getInputStream(resource));
+    }
+
+    @Override
+    public void save(IResource resource, Object object) throws Exception {
+
+        File directory = getFile(resource).getParentFile();
+
+        if(!directory.exists())
+            directory.mkdirs();
+
+
+        Serializer.serialize(object, getOutputStream(resource));
+
+    }
+
+    @Override
+    public InputStream getInputStream(IResource resource) throws Exception {
+        return new FileInputStream(getFile(resource));
+    }
+
+    @Override
+    public OutputStream getOutputStream(IResource resource) throws Exception {
+        File file = getFile(resource);
+        File directory = file.getParentFile();
+
+        if(!directory.exists())
+            directory.mkdirs();
+
+        return new FileOutputStream(file);
+    }
+
+    @Override
+    public boolean isContainer(IResource resource) throws Exception {
+        return getFile(resource).isDirectory();
+    }
+
+    private File getFile(IResource fileResource) {
+        String tenantBasePath = getTenantBasePath();
+
+        return new File(tenantBasePath
+                + fileResource.getPath());
+    }
+
+
+    @Override
+    public void move(IResource src, IContainer container) throws IOException {
+        Path source = getFile(src).toPath();
+        Path target = new File(getTenantBasePath() + container.getPath() + "/" + src.getName()).toPath();
+        Files.move(source, target, REPLACE_EXISTING);
+    }
+}
---
+++ uengine-resource-manager/src/main/java/org/uengine/modeling/resource/Storage.java
@@ -0,0 +1,34 @@
+package org.uengine.modeling.resource;
+
+import org.uengine.modeling.IModel;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.List;
+
+/**
+ * Created by jangjinyoung on 15. 7. 12..
+ */
+public interface Storage {
+    public void delete(IResource resource) throws IOException;
+    public void rename(IResource resource, String newName);
+    public void copy(IResource src, String desPath) throws Exception;
+    public void move(IResource src, IContainer container) throws IOException;
+    public List<IResource> listFiles(IContainer containerResource) throws Exception;
+
+    public void createFolder(IContainer containerResource) throws Exception;
+
+
+    public boolean exists(IResource resource) throws Exception;
+
+    Object getObject(IResource resource) throws Exception;
+
+    public void save(IResource resource, Object object) throws Exception;
+
+    public InputStream getInputStream(IResource resource) throws Exception;
+    public OutputStream getOutputStream(IResource resource) throws Exception;
+
+
+    boolean isContainer(IResource resource) throws Exception;
+}
