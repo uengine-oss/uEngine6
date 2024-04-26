f424235b126a06bd545b3b3d02abd05d0c1aa20d
sooheon45 <sooheon45@uengine.org>
2024-04-26 17:44:39 +0900

    완료 task 페이지네이션 작업

--- process-service/src/main/java/org/uengine/five/repository/WorklistRepository.java
+++ process-service/src/main/java/org/uengine/five/repository/WorklistRepository.java
@@ -2,12 +2,14 @@ package org.uengine.five.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 //import org.metaworks.multitenancy.persistence.MultitenantRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.uengine.five.entity.ProcessInstanceEntity;
 import org.uengine.five.entity.WorklistEntity;
 
 /**
@@ -33,8 +35,13 @@ public interface WorklistRepository extends JpaRepository<WorklistEntity, Long> 
     // 'PENDING')")
     // public List<WorklistEntity> findPending();
 
-    @Query("select wl from WorklistEntity wl where (wl.endpoint = ?#{principal.userId} or wl.endpoint in ?#{principal.scopes}) and (wl.status = 'COMPLETED')")
-    public List<WorklistEntity> findCompleted(Pageable pageable);
+    // @Query("select wl from WorklistEntity wl where (wl.endpoint = ?#{principal.userId} or wl.endpoint in ?#{principal.scopes}) and (wl.status = 'COMPLETED')")
+    // public List<WorklistEntity> findCompleted(Pageable pageable);
+
+    @Query("select pi from WorklistEntity pi " +
+            "where (pi.endpoint = ?#{principal.userId} or pi.endpoint in ?#{principal.scopes}) " +
+            "and (pi.status = 'COMPLETED')" )
+    Page<WorklistEntity> findCompleted(Pageable pageable);
 
     @Query("select wl from WorklistEntity wl where (wl.instId = :instId and wl.status = 'COMPLETED') order by wl.endDate ")
     public List<WorklistEntity> findWorkListByInstId(@Param(value = "instId") Long instId);
