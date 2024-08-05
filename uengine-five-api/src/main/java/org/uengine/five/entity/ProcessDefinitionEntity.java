55a73b9e49145daba881b83931d61bc6981b9a68
Administrator <admin@example.com>
2024-08-05 17:12:17 +0900

    ProcessDefinitionEntity commit

---
+++ uengine-five-api/src/main/java/org/uengine/five/entity/ProcessDefinitionEntity.java
@@ -0,0 +1,44 @@
+package org.uengine.five.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "DEF_INFO")
+public class ProcessDefinitionEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String name;
+    String path;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    
+}
