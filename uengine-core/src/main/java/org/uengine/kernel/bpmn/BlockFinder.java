17c9d03476ecd56f5ad3a342f314e1bcdb4dc5b9
m6023m <m6023m@naver.com>
2024-08-23 15:43:04 +0900

    fix infinite loop in gateway blockfinder

--- uengine-core/src/main/java/org/uengine/kernel/bpmn/BlockFinder.java
+++ uengine-core/src/main/java/org/uengine/kernel/bpmn/BlockFinder.java
@@ -8,23 +8,24 @@ import java.util.*;
 public class BlockFinder {
 
     Activity joinActivity;
-        public Activity getJoinActivity() {
-            return joinActivity;
-        }
 
-        public void setJoinActivity(Activity joinActivity) {
-            this.joinActivity = joinActivity;
-        }
+    public Activity getJoinActivity() {
+        return joinActivity;
+    }
 
+    public void setJoinActivity(Activity joinActivity) {
+        this.joinActivity = joinActivity;
+    }
 
     List<Activity> blockMembers = new ArrayList<Activity>();
-        public List<Activity> getBlockMembers() {
-            return blockMembers;
-        }
+
+    public List<Activity> getBlockMembers() {
+        return blockMembers;
+    }
 
     int branch = 1;
 
-    private BlockFinder(Activity joinActivity){
+    private BlockFinder(Activity joinActivity) {
         setJoinActivity(joinActivity);
 
         visitForDepthAndVisitCountSetting(joinActivity);
@@ -32,41 +33,41 @@ public class BlockFinder {
         findBlockMembers();
     }
 
-//    protected void find(Activity activity){
-//        branch += (activity.getIncomingTransitions().size()-1);
-//
-//        //Since queue is a interface
-//        Queue<Activity> queue = new LinkedList<Activity>();
-//
-//        if(activity == null) return;
-//
-//        //Adds to end of queue
-//        queue.add(activity);
-//
-//        while(!queue.isEmpty())
-//        {
-//            //removes from front of queue
-//            Activity r = queue.remove();
-//
-//            blockMembers.put(r.getTracingTag(), r);
-//
-//            //Visit child first before grandchild
-//            for(Transition transition : r.getIncomingTransitions())
-//            {
-//                Activity sourceActivity = transition.getSourceActivity();
-//
-//                if(!blockMembers.containsKey(sourceActivity.getTracingTag()))
-//                {
-//                    blockMembers.put(sourceActivity.getTracingTag(), sourceActivity);
-//
-//                    queue.add(sourceActivity);
-//                }
-//            }
-//
-//            if(queue.size()==1) return;
-//        }
-//
-//    }
+    // protected void find(Activity activity){
+    // branch += (activity.getIncomingTransitions().size()-1);
+    //
+    // //Since queue is a interface
+    // Queue<Activity> queue = new LinkedList<Activity>();
+    //
+    // if(activity == null) return;
+    //
+    // //Adds to end of queue
+    // queue.add(activity);
+    //
+    // while(!queue.isEmpty())
+    // {
+    // //removes from front of queue
+    // Activity r = queue.remove();
+    //
+    // blockMembers.put(r.getTracingTag(), r);
+    //
+    // //Visit child first before grandchild
+    // for(Transition transition : r.getIncomingTransitions())
+    // {
+    // Activity sourceActivity = transition.getSourceActivity();
+    //
+    // if(!blockMembers.containsKey(sourceActivity.getTracingTag()))
+    // {
+    // blockMembers.put(sourceActivity.getTracingTag(), sourceActivity);
+    //
+    // queue.add(sourceActivity);
+    // }
+    // }
+    //
+    // if(queue.size()==1) return;
+    // }
+    //
+    // }
 
     Integer depth = 0;
     Map<Integer, List<Activity>> activitiesByDistanceMap = new HashMap<Integer, List<Activity>>();
@@ -74,33 +75,34 @@ public class BlockFinder {
     Map<String, Integer> visitCount = new HashMap<String, Integer>();
     Stack<Activity> visitActivityStack = new Stack<Activity>();
 
-    protected void visitForDepthAndVisitCountSetting(Activity activity){
+    protected void visitForDepthAndVisitCountSetting(Activity activity) {
 
-        depth ++;
+        depth++;
         visitActivityStack.push(activity);
 
-        for(SequenceFlow sequenceFlow : activity.getIncomingSequenceFlows()) {
+        for (SequenceFlow sequenceFlow : activity.getIncomingSequenceFlows()) {
             Activity sourceActivity = sequenceFlow.getSourceActivity();
 
-            if(sourceActivity == null) continue;
+            if (sourceActivity == null)
+                continue;
 
-            if(visitActivityStack.contains(sourceActivity)){ //there are some feedback activity!
+            if (visitActivityStack.contains(sourceActivity)) { // there are some feedback activity!
 
                 Activity theFirstIncomingActivity = visitActivityStack.get(1);
 
-                //find out which link is the feedback link
-                for(SequenceFlow sequenceFlowToSourceActivity : theFirstIncomingActivity.getOutgoingSequenceFlows()){
-                    if(sequenceFlowToSourceActivity.getTargetActivity().equals(sourceActivity)
-                            || sequenceFlowToSourceActivity.getTargetActivity().equals(activity)){
-                        sequenceFlowToSourceActivity.setFeedback(true); //mark as feedback link
+                // find out which link is the feedback link
+                for (SequenceFlow sequenceFlowToSourceActivity : theFirstIncomingActivity.getOutgoingSequenceFlows()) {
+                    if (sequenceFlowToSourceActivity.getTargetActivity().equals(sourceActivity)
+                            || sequenceFlowToSourceActivity.getTargetActivity().equals(activity)) {
+                        sequenceFlowToSourceActivity.setFeedback(true); // mark as feedback link
                     }
                 }
 
-                continue; //cancel the already visited previous activity (feedback)
+                continue; // cancel the already visited previous activity (feedback)
             }
 
             Integer visitCountForThisActivity = 0;
-            if(visitCount.containsKey(sourceActivity.getTracingTag())){
+            if (visitCount.containsKey(sourceActivity.getTracingTag())) {
                 visitCountForThisActivity = visitCount.get(sourceActivity.getTracingTag());
             }
 
@@ -108,20 +110,32 @@ public class BlockFinder {
 
             visitCount.put(sourceActivity.getTracingTag(), visitCountForThisActivity);
 
-            visitForDepthAndVisitCountSetting(sourceActivity); //<-- point of recursive
+            visitForDepthAndVisitCountSetting(sourceActivity); // <-- point of recursive
 
             distancesByActivity.put(sourceActivity.getTracingTag(), depth);
 
         }
 
-        depth --;
+        depth--;
         visitActivityStack.pop();
     }
 
-    public void visitToLineUp(Activity activity){
+    public void visitToLineUp(Activity activity) {
+        Set<Activity> visitedActivities = new HashSet<>();
 
-        for(SequenceFlow sequenceFlow : activity.getIncomingSequenceFlows()) {
-            if(sequenceFlow.isFeedback()) continue; //ignore feedback link to avoid infinite loop
+        visitToLineUpInternal(activity, visitedActivities);
+    }
+
+    private void visitToLineUpInternal(Activity activity, Set<Activity> visitedActivities) {
+        if (visitedActivities.contains(activity)) {
+            return;
+        }
+
+        visitedActivities.add(activity);
+
+        for (SequenceFlow sequenceFlow : activity.getIncomingSequenceFlows()) {
+            if (sequenceFlow.isFeedback())
+                continue;
 
             Activity sourceActivity = sequenceFlow.getSourceActivity();
 
@@ -131,33 +145,34 @@ public class BlockFinder {
                 activitiesByDistanceMap.put(distanceOfThis, new ArrayList<Activity>());
             }
 
-            if(!activitiesByDistanceMap.get(distanceOfThis).contains(sourceActivity))
+            if (!activitiesByDistanceMap.get(distanceOfThis).contains(sourceActivity)) {
                 activitiesByDistanceMap.get(distanceOfThis).add(sourceActivity);
+            }
 
-            visitToLineUp(sourceActivity);
+            visitToLineUpInternal(sourceActivity, visitedActivities);
         }
-
     }
 
-    protected void findBlockMembers(){
+    protected void findBlockMembers() {
         blockMembers = new ArrayList<Activity>();
         int branch = joinActivity.getIncomingSequenceFlows().size();
 
-        for(int i=1; activitiesByDistanceMap.containsKey(i) && activitiesByDistanceMap.get(i).size()>0; i++){
+        for (int i = 1; activitiesByDistanceMap.containsKey(i) && activitiesByDistanceMap.get(i).size() > 0; i++) {
 
             List<Activity> activitiesInDepth = activitiesByDistanceMap.get(i);
-            if(activitiesInDepth.size()==1){
+            if (activitiesInDepth.size() == 1) {
 
                 Activity activity = activitiesInDepth.get(0);
                 blockMembers.add(activity);
                 int visitedCountForThis = visitCount.get(activity.getTracingTag());
 
-                if(branch == visitedCountForThis){  // stops when the number of splitted branch is same with visited count for the activity
+                if (branch == visitedCountForThis) { // stops when the number of splitted branch is same with visited
+                                                     // count for the activity
                     break;
                 }
-            }else {
-                for (Activity activity : activitiesInDepth){
-                    branch += activity.getIncomingSequenceFlows().size() -1;
+            } else {
+                for (Activity activity : activitiesInDepth) {
+                    branch += activity.getIncomingSequenceFlows().size() - 1;
 
                     blockMembers.add(activity);
                 }
@@ -165,12 +180,11 @@ public class BlockFinder {
 
         }
 
-
     }
 
-
-    public static List<Activity> getPossibleBlockMembers(List<Activity> blockMembers, ProcessInstance instance) throws Exception {
-        Activity theLastBlockMember = blockMembers.get(blockMembers.size()-1);
+    public static List<Activity> getPossibleBlockMembers(List<Activity> blockMembers, ProcessInstance instance)
+            throws Exception {
+        Activity theLastBlockMember = blockMembers.get(blockMembers.size() - 1);
         List possibleBlockMembers = new ArrayList<Activity>();
 
         possibleBlockMembers.add(theLastBlockMember);
@@ -180,22 +194,19 @@ public class BlockFinder {
         return possibleBlockMembers;
     }
 
-    private static void visitForPossibleNodes(Activity activity, List<Activity> blockMembers, ProcessInstance instance, List<Activity>possibleNodes) throws Exception {
+    private static void visitForPossibleNodes(Activity activity, List<Activity> blockMembers, ProcessInstance instance,
+            List<Activity> possibleNodes) throws Exception {
 
-        for(Activity next : activity.getPossibleNextActivities(instance, "")){
-            if(blockMembers.contains(next) && !possibleNodes.contains(next)){
+        for (Activity next : activity.getPossibleNextActivities(instance, "")) {
+            if (blockMembers.contains(next) && !possibleNodes.contains(next)) {
                 possibleNodes.add(next);
                 visitForPossibleNodes(next, blockMembers, instance, possibleNodes);
             }
         }
     }
 
-
-    public static List<Activity> getBlockMembers(Activity joinActivity){
+    public static List<Activity> getBlockMembers(Activity joinActivity) {
         return new BlockFinder(joinActivity).getBlockMembers();
     }
-
-
-
 
 }
