34e61af14734afb5dba03c3e253c4fb010b23c3e
m6023m <m6023m@naver.com>
2024-11-14 15:20:47 +0900

    Add getValues

--- uengine-core/src/main/java/org/uengine/kernel/ProcessVariableValue.java
+++ uengine-core/src/main/java/org/uengine/kernel/ProcessVariableValue.java
@@ -7,16 +7,16 @@ import java.util.*;
  * @author Jinyoung Jang
  */
 
-public class ProcessVariableValue implements java.io.Serializable, Cloneable{
+public class ProcessVariableValue implements java.io.Serializable, Cloneable {
 	private static final long serialVersionUID = org.uengine.kernel.GlobalContext.SERIALIZATION_UID;
-	
-    int cursor;
-    boolean isSingle = true;
-    
-    String name;
-    
-    //for multiple role binding
-    private ArrayList values = new ArrayList();
+
+	int cursor;
+	boolean isSingle = true;
+
+	String name;
+
+	// for multiple role binding
+	private ArrayList values = new ArrayList();
 
 	public String getName() {
 		return name;
@@ -29,16 +29,15 @@ public class ProcessVariableValue implements java.io.Serializable, Cloneable{
 	public Serializable getValue(ProcessInstance instance) {
 		final Serializable sourceValue = getValue();
 
-		if(sourceValue instanceof VariablePointer){
+		if (sourceValue instanceof VariablePointer) {
 			try {
 
-				return
-						(Serializable) new InRootExecutionScope(){
-                            @Override
-                            public Object logic(ProcessInstance instance) throws Exception {
-                                return ((VariablePointer) sourceValue).getValue(instance);
-                            }
-                        }.run(instance);
+				return (Serializable) new InRootExecutionScope() {
+					@Override
+					public Object logic(ProcessInstance instance) throws Exception {
+						return ((VariablePointer) sourceValue).getValue(instance);
+					}
+				}.run(instance);
 			} catch (Exception e) {
 				throw new RuntimeException(e);
 			}
@@ -48,15 +47,22 @@ public class ProcessVariableValue implements java.io.Serializable, Cloneable{
 	}
 
 	public Serializable getValue() {
-		if(values.size() == 0) return null;
-		
-		Serializable sourceValue = (Serializable)values.get(getCursor());
+		if (values.size() == 0)
+			return null;
+
+		Serializable sourceValue = (Serializable) values.get(getCursor());
 
 		return sourceValue;
 	}
 
+	public Serializable getValues() {
+		if (values.size() == 0)
+			return null;
+		return values;
+	}
+
 	public void setValue(Serializable value) {
-		if(getCursor() >= values.size())
+		if (getCursor() >= values.size())
 			values.add(value);
 		else
 			values.set(getCursor(), value);
@@ -67,110 +73,108 @@ public class ProcessVariableValue implements java.io.Serializable, Cloneable{
 	 * @param value
 	 */
 	public void setValue(Object value) {
-		setValue((Serializable)value);
+		setValue((Serializable) value);
 	}
 
-    public synchronized boolean next(){
-    	if(values==null)
-    		return false;
-    	else if(cursor >= (values.size()-1))
-    		return false;	
-    	    	
-    	cursor++;
-    	
-    	return true;
-    }
-    
-    public synchronized void moveToAdd(){
+	public synchronized boolean next() {
+		if (values == null)
+			return false;
+		else if (cursor >= (values.size() - 1))
+			return false;
+
+		cursor++;
+
+		return true;
+	}
+
+	public synchronized void moveToAdd() {
 		setCursor(values.size());
 	}
-    
-    public synchronized int size(){
-    	return values.size();
-    }
-    
-	public synchronized int getCursor(){
+
+	public synchronized int size() {
+		return values.size();
+	}
+
+	public synchronized int getCursor() {
 		return cursor;
 	}
-	
-	public synchronized void setCursor(int c){
-		if(c>=0 && c<=size())
+
+	public synchronized void setCursor(int c) {
+		if (c >= 0 && c <= size())
 			cursor = c;
 	}
 
 	public synchronized void remove() {
-		if(values==null) return;
-		
+		if (values == null)
+			return;
+
 		values.remove(getCursor());
-		
-		if(getCursor() >= size())
+
+		if (getCursor() >= size())
 			setCursor(size() - 1);
 	}
-	
-	public void beforeFirst(){
+
+	public void beforeFirst() {
 		setCursor(0);
 	}
 
-
-	public static void main(String [] args) throws Exception{
+	public static void main(String[] args) throws Exception {
 		ProcessVariableValue pvv = new ProcessVariableValue();
-		
-		
+
 		pvv.setName("testval");
 		pvv.setValue("value1");
-		
+
 		System.out.println(pvv.getValue());
-		
+
 		pvv.moveToAdd();
 		pvv.setValue("value2");
 		pvv.moveToAdd();
 		pvv.setValue("value3");
 		pvv.moveToAdd();
 		pvv.setValue("value4");
-		
+
 		GlobalContext.serialize(pvv, System.out, ProcessVariableValue.class);
-		
+
 		pvv.beforeFirst();
-		
+
 		ProcessInstance instance = new DefaultProcessInstance();
 		ProcessDefinition definition = new ProcessDefinition();
 		instance.setProcessDefinition(definition);
 		instance.set("", pvv);
-		
+
 		pvv = instance.getMultiple("", "testval");
-		
-		do{
-			System.out.println(" value = " + pvv.getValue());			
-		}while(pvv.next());		
-		
+
+		do {
+			System.out.println(" value = " + pvv.getValue());
+		} while (pvv.next());
+
 		pvv.beforeFirst();
 		pvv.remove();
-		do{
-			System.out.println(" value = " + pvv.getValue());			
-		}while(pvv.next());		
-		
+		do {
+			System.out.println(" value = " + pvv.getValue());
+		} while (pvv.next());
+
 	}
-	
+
 	public String toString() {
-		//if (size() > 1) {
-			int currentCursor = getCursor();
+		// if (size() > 1) {
+		int currentCursor = getCursor();
 
-			beforeFirst();
+		beforeFirst();
 
-			StringBuilder names = new StringBuilder();
-			do {
-				if (names.length() > 0) names.append(", ");
-				names.append(String.valueOf(getValue()));
-			} while (next());
+		StringBuilder names = new StringBuilder();
+		do {
+			if (names.length() > 0)
+				names.append(", ");
+			names.append(String.valueOf(getValue()));
+		} while (next());
 
+		setCursor(currentCursor);
 
-			setCursor(currentCursor);
-
-			return names.toString();
-//		} else {
-//			return String.valueOf(getValue());
-//		}
+		return names.toString();
+		// } else {
+		// return String.valueOf(getValue());
+		// }
 	}
-
 
 }
