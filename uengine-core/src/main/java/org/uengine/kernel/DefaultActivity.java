ecb668966ec4ffb0eb378478fc7d668175cea73c
sooheon45 <sooheon45@uengine.org>
2026-02-06 15:34:04 +0900

    인스턴스 필요한 task id 추가(script, service) 패턴처리

--- uengine-core/src/main/java/org/uengine/kernel/DefaultActivity.java
+++ uengine-core/src/main/java/org/uengine/kernel/DefaultActivity.java
@@ -11,6 +11,8 @@ import org.uengine.util.UEngineUtil;
 public class DefaultActivity extends Activity{
 	private static final long serialVersionUID = org.uengine.kernel.GlobalContext.SERIALIZATION_UID;
 
+	public static final String PVKEY_TASKID = "_task id";
+
 	public DefaultActivity(String name){
 		setName(name);
 		
@@ -28,14 +30,40 @@ public class DefaultActivity extends Activity{
 
 
 	String document;
-		public String getDocument() {
-			return document;
-		}
-		public void setDocument(String document) {
-			this.document = document;
-		}
+
+	public String getDocument() {
+		return document;
+	}
+	public void setDocument(String document) {
+		this.document = document;
+	}
 
 
+	public void setTaskIds(ProcessInstance instance, String[] taskIds) throws Exception {
+		StringBuffer taskId = new StringBuffer();
+		if (taskIds != null) {
+			for (int i = 0; i < taskIds.length; i++) {
+				if (taskId.length() > 0)
+					taskId.append(",");
+				taskId.append(taskIds[i]);
+			}
+		}
+
+		instance.setProperty(getTracingTag(), PVKEY_TASKID, (taskId.length() > 0 ? taskId.toString() : null));
+	}
+
+	public String[] getTaskIds(ProcessInstance instance) throws Exception {
+		if (instance == null)
+			return null;
+		String taskId = (String) instance.getProperty(getTracingTag(), PVKEY_TASKID);
+		String[] taskIds = null;
+		if (taskId != null && taskId.trim().length() > 0)
+			taskIds = taskId.split(",");
+		else
+			taskIds = null;
+
+		return taskIds;
+	}
 
 }
 
